e32df8d07b5708e7d305ba95e99582d46eb9c334
tobikster <tobikster@gmail.com>
2012-11-14 19:42:07 +0100

    Utworzenie repozytorium

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Paweł
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}

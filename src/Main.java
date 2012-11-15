7d11ca96eff287796596f584e3da76961cd02450
Zjamnik <zjamnik@gmail.com>
2012-11-15 12:41:59 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World2!");
     }
 }

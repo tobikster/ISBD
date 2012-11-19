1b5fb220c52ddebbf806d0f1ab167ad598c462b1
tobikster <tobikster@tobikster-vwin7>
2012-11-19 22:40:00 +0100

    Naprawianie DatabaseException

---
+++ src/articles/c/ArticleAttributeValueValidator.java
@@ -0,0 +1,26 @@
+package articles.c;
+
+public class ArticleAttributeValueValidator
+{
+  // <editor-fold defaultstate="collapsed" desc="Object variables">
+
+  // </editor-fold>
+
+  // <editor-fold defaultstate="collapsed" desc="Creating object">
+
+  // </editor-fold>
+
+  // <editor-fold defaultstate="collapsed" desc="Object PRIVATE methods">
+
+  // </editor-fold>
+
+  // <editor-fold defaultstate="collapsed" desc="Object PUBLIC methods">
+  // <editor-fold defaultstate="collapsed" desc="Getters">
+  
+  // </editor-fold>
+  // <editor-fold defaultstate="collapsed" desc="Setters">
+  
+  // </editor-fold>
+  
+  // </editor-fold>
+}

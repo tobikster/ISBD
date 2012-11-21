e1402ace79485268071cdbfb7b1f1cbbc6c5a9b6
MRKACZOR <MRKACZOR@MRKACZOR-PC>
2012-11-21 16:28:10 +0100

    Zajęcia

--- src/core/c/TablePagination.java
+++ src/core/c/TablePagination.java
@@ -10,12 +10,12 @@ package core.c;
  */
 public class TablePagination
 {
-	private Object[] tableData;
+	private Object[][] tableData;
 	private int rowsPerPage;
 	private int currentPage;
 	private int pageCount;
 	
-	public TablePagination(Object[] tableData, int rowsPerPage)
+	public TablePagination(Object[][] tableData, int rowsPerPage)
 	{
 		this.tableData = tableData;
 		this.rowsPerPage = rowsPerPage;
@@ -23,7 +23,7 @@ public class TablePagination
 		pageCount = (int)Math.ceil((double)(tableData.length) / (double)(rowsPerPage));
 	}
 
-	public Object[] getTableData()
+	public Object[][] getTableData()
 	{
 		return tableData;
 	}
@@ -43,7 +43,7 @@ public class TablePagination
 		return pageCount;
 	}
 
-	public void setTableData(Object[] tableData)
+	public void setTableData(Object[][] tableData)
 	{
 		this.tableData = tableData;
 		pageCount = (int)Math.ceil((tableData.length * 1.0) / (rowsPerPage * 1.0));
@@ -55,14 +55,14 @@ public class TablePagination
 		pageCount = (int)Math.ceil((tableData.length * 1.0) / (rowsPerPage * 1.0));
 	}
 	
-	public Object[]getCurrentPageData() throws PageNumberException
+	public Object[][] getCurrentPageData() throws PageNumberException
 	{
 		if (currentPage < 0 || currentPage > pageCount-1)
 		{
 			throw new PageNumberException("Numer strony (" + currentPage + ") poza zakresem (" + 0 + "-" + (pageCount-1) + ").");
 		}
 		
-		Object[] currentPageData = new Object[currentPage == pageCount-1 ? (tableData.length % rowsPerPage == 0 ? rowsPerPage : tableData.length % rowsPerPage) : rowsPerPage][];
+		Object[][] currentPageData = new Object[currentPage == pageCount-1 ? (tableData.length % rowsPerPage == 0 ? rowsPerPage : tableData.length % rowsPerPage) : rowsPerPage][];
 		
 		for(int i = 0; currentPage * rowsPerPage + i < tableData.length && i < rowsPerPage; ++i)
 		{
@@ -72,21 +72,21 @@ public class TablePagination
 		return currentPageData;
 	}
 	
-	public Object[] getNextPage() throws PageNumberException
+	public Object[][] getNextPage() throws PageNumberException
 	{
 		++currentPage;
 		
 		return getCurrentPageData();
 	}
 	
-	public Object[] getPreviousPage() throws PageNumberException
+	public Object[][] getPreviousPage() throws PageNumberException
 	{
 		--currentPage;
 		
 		return getCurrentPageData();
 	}
 	
-	public Object[] getPageData(int page) throws PageNumberException
+	public Object[][] getPageData(int page) throws PageNumberException
 	{
 		currentPage = page;
 		

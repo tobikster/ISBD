6ce2eb2a55dbba3cc838b02781a6d055144c9c1e
Zjamnik <zjamnik@gmail.com>
2012-11-23 16:52:51 +0100

    Pagination

--- src/core/c/TablePagination.java
+++ src/core/c/TablePagination.java
@@ -11,88 +11,79 @@ import java.util.List;
  *
  * @author Zjamnik
  */
-public class TablePagination <T>
-{
+public class TablePagination<T> {
+
 	private List<T> tableData;
 	private int rowsPerPage;
 	private int currentPage;
 	private int pageCount;
-	
-	public TablePagination(List<T> tableData, int rowsPerPage)
-	{
+
+	public TablePagination(List<T> tableData, int rowsPerPage) {
 		this.tableData = tableData;
 		this.rowsPerPage = rowsPerPage;
 		currentPage = 0;
-		pageCount = (int)Math.ceil((double)(tableData.size()) / (double)(rowsPerPage));
+		pageCount = (int) Math.ceil((double) (tableData.size()) / (double) (rowsPerPage));
 	}
 
-	public List<T> getTableData()
-	{
+	public List<T> getTableData() {
 		return tableData;
 	}
 
-	public int getRowsPerPage()
-	{
+	public int getRowsPerPage() {
 		return rowsPerPage;
 	}
 
-	public int getCurrentPage()
-	{
+	public int getCurrentPage() {
 		return currentPage;
 	}
 
-	public int getPageCount()
-	{
+	public int getPageCount() {
 		return pageCount;
 	}
 
-	public void setTableData(List<T> tableData)
-	{
+	public void setTableData(List<T> tableData) {
+		currentPage = 0;
 		this.tableData = tableData;
-		pageCount = (int)Math.ceil((tableData.size() * 1.0) / (rowsPerPage * 1.0));
+		pageCount = (int) Math.ceil((tableData.size() * 1.0) / (rowsPerPage * 1.0));
 	}
 
-	public void setRowsPerPage(int rowsPerPage)
-	{
+	public void setRowsPerPage(int rowsPerPage) {
+		currentPage = 0;
 		this.rowsPerPage = rowsPerPage;
-		pageCount = (int)Math.ceil((tableData.size() * 1.0) / (rowsPerPage * 1.0));
+		pageCount = (int) Math.ceil((tableData.size() * 1.0) / (rowsPerPage * 1.0));
 	}
-	
-	public List<T> getCurrentPageData()
-	{
-		if (currentPage < 0 || currentPage > pageCount-1)
-		{
-//			throw new PageNumberException("Numer strony (" + currentPage + ") poza zakresem (" + 0 + "-" + (pageCount-1) + ").");
-		}
-		
-		List<T> currentPageData = new LinkedList<T>();
-		
-		for(int i = 0; currentPage * rowsPerPage + i < tableData.size() && i < rowsPerPage; ++i)
-		{
+
+	public List<T> getCurrentPageData() {
+		List<T> currentPageData = new LinkedList<>();
+
+		for (int i = 0; currentPage * rowsPerPage + i < tableData.size() && i < rowsPerPage; ++i) {
 			currentPageData.add(tableData.get(currentPage * rowsPerPage + i));
 		}
-		
+
 		return currentPageData;
 	}
-	
-	public List<T> getNextPage()
-	{
-		++currentPage;
-		
+
+	public List<T> getNextPage() {
+		if ((currentPage + 1) < pageCount) {
+			++currentPage;
+		}
+
 		return getCurrentPageData();
 	}
-	
-	public List<T> getPreviousPage()
-	{
-		--currentPage;
-		
+
+	public List<T> getPreviousPage() {
+		if ((currentPage - 1) >= 0) {
+			--currentPage;
+		}
+
 		return getCurrentPageData();
 	}
-	
-	public List<T> getPageData(int page)
-	{
-		currentPage = page;
-		
+
+	public List<T> getPageData(int page) {
+		if (currentPage >= 0 || currentPage < pageCount) {
+			currentPage = page;
+		}
+
 		return getCurrentPageData();
 	}
 }
